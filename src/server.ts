98b0e98ad39fb31edd7bdfc8c9f488c2519f8fcf
Victor Onibudo <tonibudo@lagetronix.com>
2022-06-02 18:39:59 +0100

    create a car api

--- src/server.ts
+++ src/server.ts
@@ -71,12 +71,44 @@ import { Car, cars as cars_list } from './cars';
   // @TODO Add an endpoint to GET a list of cars
   // it should be filterable by make with a query paramater
 
+  app.get("/cars", (req: Request, res: Response) => {
+    let {make} = req.query
+
+    if(make){
+      return res.status(200).send(cars_list.filter(x => {
+        if(x.make == make) return x
+      }))
+    }
+
+    res.status(200).send(cars_list.sort(x => x.id))
+  })
+
   // @TODO Add an endpoint to get a specific car
   // it should require id
   // it should fail gracefully if no matching car is found
 
+  app.get("/cars/:id", (req: Request, res: Response) => {
+    let { id } = req.params;
+
+    let car = cars_list.find(x => x.id == parseInt(id))
+
+    return !car ? res.status(404).send(`car with id ${id} not found`) : res.status(200).send(car)
+  })
+
   /// @TODO Add an endpoint to post a new car to our list
   // it should require id, type, model, and cost
+
+  app.post("/cars", (req: Request, res: Response) => {
+    const { make, type, model, cost } = req.body;
+    
+    if(!make || !type || !model || !cost){
+      res.status(400).send(`name,type,model or cost can't be null`)
+    }
+
+    cars_list.push(req.body)
+
+    return res.status(201).send(`car added succesfully`)
+  })
 
   // Start the Server
   app.listen( port, () => {
